472ccee701d9ed7cd5f9648b0d633b77b2c94d89
16602724475 <1714797713@qq.com>
2022-11-05 12:20:43 +0800

    layout init

---
+++ nuxt.config.js
@@ -0,0 +1,56 @@
+export default {
+  router: {
+    linkActiveClass: 'active',
+    // 自定义路由表规则
+    extendRoutes(routes, resolve) {
+      // 清除pagse默认的路由表
+      routes.splice(0);
+      routes.push(
+        ...[
+          {
+            path: '/',
+            component: resolve(__dirname, 'pages/layout/index.vue'),
+            children: [
+              {
+                path: '',
+                name: 'home',
+                component: resolve(__dirname, 'pages/home/index.vue'),
+              },
+              {
+                path: '/login',
+                name: 'login',
+                component: resolve(__dirname, 'pages/login/index.vue'),
+              },
+              {
+                path: '/register',
+                name: 'register',
+                component: resolve(__dirname, 'pages/login/index.vue'),
+              },
+              {
+                path: '/profile/:username',
+                name: 'profile',
+                component: resolve(__dirname, 'pages/profile/index.vue'),
+              },
+              
+              {
+                path: '/settings',
+                name: 'settings',
+                component: resolve(__dirname, 'pages/settings/index.vue'),
+              },
+              {
+                path: '/editor',
+                name: 'editor',
+                component: resolve(__dirname, 'pages/editor/index.vue'),
+              },
+              {
+                path: '/article/:slug',
+                name: 'article',
+                component: resolve(__dirname, 'pages/article/index.vue'),
+              },
+            ], 
+          },
+        ]
+      );
+    },
+  },
+};
---
+++ pages/layout/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <!-- herder -->
+    <nav class="navbar navbar-light">
+      <div class="container">
+        <!-- <a class="navbar-brand" href="index.html">conduit</a> -->
+        <nuxt-link class="navbar-brand" to="/" >Home</nuxt-link>
+        <ul class="nav navbar-nav pull-xs-right">
+          <!-- Add "active" class when you're on that page" -->
+          <li class="nav-item">
+            <!-- <a class="nav-link" href="">Home</a> -->
+            <nuxt-link class="nav-link" to="/" exact>Home</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link class="nav-link" to="/editor"
+              ><i class="ion-compose"></i>&nbsp;New Post</nuxt-link
+            >
+            <!-- <a class="nav-link" href="">
+              <i class="ion-compose"></i>&nbsp;New Post
+            </a> -->
+          </li>
+          <li class="nav-item">
+            <!-- <a class="nav-link" href="">
+              <i class="ion-gear-a"></i>&nbsp;Settings
+            </a> -->
+            <nuxt-link class="nav-link" to="/settings"
+              ><i class="ion-gear-a"></i>&nbsp;Settings</nuxt-link
+            >
+          </li>
+          <li class="nav-item">
+            <!-- <a class="nav-link" href="">Sign up</a> -->
+            <nuxt-link class="nav-link" to="/register">Sign up</nuxt-link>
+          </li><li class="nav-item">
+            <!-- <a class="nav-link" href="">Sign up</a> -->
+            <nuxt-link class="nav-link" to="/login">Sign in</nuxt-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <!-- child router  -->
+    <nuxt-child></nuxt-child>
+
+    <!-- footer -->
+    <footer>
+      <div class="container">
+        <a href="/" class="logo-font">conduit</a>
+        <span class="attribution">
+          An interactive learning project from
+          <a href="https://thinkster.io">Thinkster</a>. Code &amp; design
+          licensed under MIT.
+        </span>
+      </div>
+    </footer>
+  </div>
+</template>
+<script>
+export default {
+  naem: 'LayoutIndex',
+};
+</script>
+<style lang=""></style>
